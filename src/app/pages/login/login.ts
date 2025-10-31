a743b3d1c0e12b64eed01c01b67f8d0fe82fcf89
unknown <matheusleao585@gmail.com>
2025-10-31 16:53:27 -0300

    :sparkles: feat(pages): login page

---
+++ src/app/pages/login/login.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { InputFieldComponent } from "../../components/main-input-field/input-field.component";
+
+@Component({
+  selector: 'app-login',
+  imports: [InputFieldComponent],
+  templateUrl: './login.html',
+  styleUrl: './login.scss',
+})
+export default class Login {
+
+}
